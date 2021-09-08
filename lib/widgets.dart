8d30a3c7c34960e5d2df593401b6c0ae0c2e8ced
ManjunathRK1251 <rkmanjunath10000@gmail.com>
2021-09-08 20:10:53 +0530

    changed the background color of search area

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -206,48 +206,51 @@ class SearchWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        showCountryPicker(
-          context: context,
-          onSelect: (Country country) {
-            // countryName = country.name;
-            print(country.name);
-          },
-          countryListTheme: CountryListThemeData(
-            backgroundColor: Color(0xfffff2f2),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(40.0),
-              topRight: Radius.circular(40.0),
-            ),
-          ),
-        );
-      },
-      child: Stack(alignment: AlignmentDirectional.center, children: [
-        SvgPicture.string(
-          '<svg viewBox="0.0 772.0 393.0 79.0" ><path transform="matrix(-1.0, 0.0, 0.0, -1.0, 405.0, 919.0)" d="M 11.99970054626465 101.9876480102539 L 11.99970054626465 68.00040435791016 L 405.0003051757812 68.00040435791016 L 405.0003051757812 101.9876327514648 C 378.3428039550781 128.0993041992188 300.44482421875 146.9996948242188 208.4999847412109 146.9996948242188 C 116.5551605224609 146.9996948242188 38.6572151184082 128.0993194580078 11.99970054626465 101.9876480102539 Z" fill="#6a3785" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>',
-          allowDrawingOutsideViewBox: true,
-          fit: BoxFit.cover,
-        ),
-        Column(
-          children: [
-            Icon(
-              Icons.search,
-              color: Colors.white,
-              size: 35,
-            ),
-            Text(
-              'SEARCH',
-              style: TextStyle(
-                fontFamily: 'Segoe UI',
-                fontSize: 26,
-                color: const Color(0xfffff2f2),
+    return SingleChildScrollView(
+      physics: NeverScrollableScrollPhysics(),
+      child: InkWell(
+        onTap: () {
+          showCountryPicker(
+            context: context,
+            onSelect: (Country country) {
+              // countryName = country.name;
+              print(country.name);
+            },
+            countryListTheme: CountryListThemeData(
+              backgroundColor: Color(0xff6A3785),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40.0),
+                topRight: Radius.circular(40.0),
               ),
-              textAlign: TextAlign.left,
             ),
-          ],
-        )
-      ]),
+          );
+        },
+        child: Stack(alignment: AlignmentDirectional.center, children: [
+          SvgPicture.string(
+            '<svg viewBox="0.0 772.0 393.0 79.0" ><path transform="matrix(-1.0, 0.0, 0.0, -1.0, 405.0, 919.0)" d="M 11.99970054626465 101.9876480102539 L 11.99970054626465 68.00040435791016 L 405.0003051757812 68.00040435791016 L 405.0003051757812 101.9876327514648 C 378.3428039550781 128.0993041992188 300.44482421875 146.9996948242188 208.4999847412109 146.9996948242188 C 116.5551605224609 146.9996948242188 38.6572151184082 128.0993194580078 11.99970054626465 101.9876480102539 Z" fill="#6a3785" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>',
+            allowDrawingOutsideViewBox: true,
+            fit: BoxFit.cover,
+          ),
+          Column(
+            children: [
+              Icon(
+                Icons.search,
+                color: Colors.white,
+                size: 35,
+              ),
+              Text(
+                'SEARCH',
+                style: TextStyle(
+                  fontFamily: 'Segoe UI',
+                  fontSize: 26,
+                  color: const Color(0xfffff2f2),
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ],
+          )
+        ]),
+      ),
     );
   }
 }

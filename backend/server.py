76606330feb803e807320d23f322d132069f730b
Gowtham-M1729 <gowtham1842001@gmail.com>
2021-09-15 23:33:47 +0530

    Added Active cases in DB

--- backend/server.py
+++ backend/server.py
@@ -1,56 +1,100 @@
-from flask import Flask
+from flask import Flask, send_file
+from flask_restful import Api, Resource, abort, marshal_with, fields, reqparse
 from flask_sqlalchemy import SQLAlchemy
 import requests
-import datetime
+import json
+import matplotlib.pyplot as plt
+import threading
+import schedule
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
-lst_countrtinfo=requests.get("https://api.covid19api.com/countries")
-lst_c=lst_countrtinfo.json()
-lst_countrysummary=(requests.get("https://api.covid19api.com/summary")).json()
-lst=lst_countrysummary["Countries"]
 
-for i in lst:
-    print(i)
-    print()
-
-class Country(db.Model):
-
+class DataModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    country = db.Column(db.String(120), nullable=False)
-    slug = db.Column(db.String(80), nullable=False)
-    iso2 = db.Column(db.String(50), nullable=False)
+    iso2 = db.Column(db.String, nullable=False)
+    country = db.Column(db.String(100), nullable=False)
+    slug = db.Column(db.String(100), nullable=False)
+    newConfirmed = db.Column(db.Integer, nullable=False)
+    newDeaths = db.Column(db.Integer, nullable=False)
+    newRecovered = db.Column(db.Integer, nullable=False)
+    totalConfirmed = db.Column(db.Integer, nullable=False)
+    totalDeaths = db.Column(db.Integer, nullable=False)
+    totalRecovered = db.Column(db.Integer, nullable=False)
+    active = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return '<country %r , slug %r, iso2 %r>' % self.country, self.slug, self.iso2
-
-class CountrySummary(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    country = db.Column(db.String(120), nullable=False)
-    countrycode = db.Column(db.String(20), nullable=False)
-    slug =  db.Column(db.String(80), nullable=False)
-    newconfirmed = db.Column(db.Integer, nullable=False)
-    totalconfirmed = db.Column(db.Integer, nullable=False)
-    newdeaths = db.Column(db.Integer,nullable=False)
-    totaldeaths = db.Column(db.Integer, nullable=False)
-    newrecovered = db.Column(db.Integer,nullable=False)
-    totalrecovered = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.String(120),nullable=False)
+        return f"Data(name = {country},iso2={iso2},slug={slug} ,newconfiremd = {newConfirmed}, newdeaths = {newDeaths}, newrecovered = {newRecovered},totalconfirmed={totalConfirmed},totalrecovered={totalRecovered},totaldeaths={totalDeaths},active = {active})"
 
 
-db.create_all()
+resource_fields = {
+    'iso2': fields.String,
+    'country': fields.String,
+    'slug': fields.String,
+    'newConfirmed': fields.Integer,
+    'totalConfirmed': fields.Integer,
+    'totalDaeths': fields.Integer,
+    'newDeaths': fields.Integer,
+    'totalRecovered': fields.Integer,
+    'newRecovered': fields.Integer,
+    'active': fields.Integer
 
-for i in lst_c:
-    u = Country(country = i['Country'], slug = i['Slug'],iso2 = i['ISO2'])
-    db.session.add(u)
-    db.session.commit()
+}
 
-for i in lst:
-    u = CountrySummary(country = i['Country'], countrycode = i['CountryCode'], slug = i['Slug'], newconfirmed = i['NewConfirmed'], totalconfirmed = i['TotalConfirmed'], newdeaths = i['NewDeaths'], totaldeaths = i['TotalDeaths'], newrecovered = i['NewRecovered'], totalrecovered = i['TotalRecovered'], date = i['Date'])
-    db.session.add(u)
-    db.session.commit()
 
-    
+def updateDatabase():
+
+    print("Updating database ....")
+    BASE = "https://api.covid19api.com/total/country/"
+    lst_countrysummary = (requests.get(
+        "https://api.covid19api.com/summary")).json()
+    lst = lst_countrysummary["Countries"]
+    countries = sorted(lst, key=lambda c: c["NewConfirmed"])
+    globaldata = lst_countrysummary['Global']
+
+    for i in lst:
+        (country, countrycode, slug, newconfirmed, totalconfirmed, newdeaths, totaldeaths, newrecovered, totalrecovered,
+         date) = i['Country'], i['CountryCode'], i['Slug'], i['NewConfirmed'], i['TotalConfirmed'], i['NewDeaths'], i['TotalDeaths'], i['NewRecovered'], i['TotalRecovered'], i['Date']
+
+        countries_info = requests.get(BASE + countrycode).json()
+        activeCases = countries_info[-1]['Active'] - \
+            countries_info[-2]['Active']
+
+        c_data = DataModel(iso2=countrycode, country=country, slug=slug, newConfirmed=newconfirmed, newDeaths=newdeaths, newRecovered=newrecovered, totalConfirmed=totalconfirmed,
+                           totalRecovered=totalrecovered, totalDeaths=totaldeaths, active=activeCases)
+        db.session.add(c_data)
+
+        print("Updated " + country)
+        db.session.commit()
+
+    print("Update complete ...")
+
+
+@app.route('/reset')
+def reset():
+    db.drop_all()
+    db.create_all()
+    return "Done"
+
+
+if __name__ == "__main__":
+
+    db.create_all()
+
+    thread = threading.Thread(target=updateDatabase)
+    thread.start()
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=updateDatabase, trigger="interval", days=1)
+    scheduler.start()
+
+    app.run(debug=True, use_reloader=False,
+            port=os.getenv("PORT"), host="0.0.0.0")
+    atexit.register(lambda: scheduler.shutdown())

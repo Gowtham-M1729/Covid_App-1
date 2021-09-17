e0a09babb1bddde6bf8fdc8a6573c8dd5e81444f
ManjunathRK1251 <rkmanjunath10000@gmail.com>
2021-09-18 00:55:54 +0530

    Add search bar for countries screen

--- lib/countryList.dart
+++ lib/countryList.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'country.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:search_choices/search_choices.dart';
 
 class CountriesList extends StatefulWidget {
   const CountriesList({Key? key}) : super(key: key);
@@ -39,6 +40,49 @@ class _CountriesListState extends State<CountriesList> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Color(0xff66466E),
+          title: // Figma Flutter Generator Rectangle3Widget - RECTANGLE
+              Stack(
+            alignment: Alignment(0.9, 0.1),
+            children: [
+              Container(
+                child: TextField(
+                  decoration: InputDecoration(
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff66466E),
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color(0xff66466E),
+                      ),
+                    ),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      backgroundColor: Colors.white),
+                ),
+                width: 374,
+                height: 44,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                  color: Color.fromRGBO(64, 22, 75, 1),
+                ),
+              ),
+              Icon(Icons.search),
+            ],
+          ),
+        ),
         backgroundColor: Color(0xff66466E),
         body: (loading)
             ? Center(
@@ -60,6 +104,7 @@ class _CountriesListState extends State<CountriesList> {
                       padding: EdgeInsets.all(20.0),
                       //color: Colors.purple,
                       child: ListView.separated(
+                        addRepaintBoundaries: false,
                         separatorBuilder: (BuildContext context, int index) {
                           return SizedBox(height: 15);
                         },

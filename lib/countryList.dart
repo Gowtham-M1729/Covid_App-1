e396f4d5ff6bc0d98628bceab814012663a97215
ManjunathRK1251 <rkmanjunath10000@gmail.com>
2021-09-18 01:29:03 +0530

    Change padding between countries,increased search bar size

--- lib/countryList.dart
+++ lib/countryList.dart
@@ -40,48 +40,51 @@ class _CountriesListState extends State<CountriesList> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Color(0xff66466E),
-          title: // Figma Flutter Generator Rectangle3Widget - RECTANGLE
-              Stack(
-            alignment: Alignment(0.9, 0.1),
-            children: [
-              Container(
-                child: TextField(
-                  decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          //color: Color(0xff66466E),
-                          ),
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color(0xff66466E),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(50.0),
+          child: AppBar(
+            elevation: 0.0,
+            backgroundColor: Color(0xff66466E),
+            title: // Figma Flutter Generator Rectangle3Widget - RECTANGLE
+                Stack(
+              alignment: Alignment(0.9, 0.1),
+              children: [
+                Container(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xff66466E),
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xff66466E),
+                        ),
                       ),
                     ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white54,
+                      fontSize: 20.0,
+                      //backgroundColor: Colors.white,
+                    ),
                   ),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white54,
-                    fontSize: 20.0,
-                    //backgroundColor: Colors.white,
+                  width: 374,
+                  height: 44,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                    color: Color.fromRGBO(64, 22, 75, 1),
                   ),
                 ),
-                width: 374,
-                height: 44,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                    bottomLeft: Radius.circular(20),
-                    bottomRight: Radius.circular(20),
-                  ),
-                  color: Color.fromRGBO(64, 22, 75, 1),
-                ),
-              ),
-              Icon(Icons.search),
-            ],
+                Icon(Icons.search),
+              ],
+            ),
           ),
         ),
         backgroundColor: Color(0xff66466E),
@@ -107,7 +110,7 @@ class _CountriesListState extends State<CountriesList> {
                       child: ListView.separated(
                         addRepaintBoundaries: false,
                         separatorBuilder: (BuildContext context, int index) {
-                          return SizedBox(height: 15);
+                          return SizedBox(height: 10);
                         },
                         itemBuilder: (context, index) {
                           return // Figma Flutter Generator Rectangle4Widget - RECTANGLE
@@ -128,7 +131,7 @@ class _CountriesListState extends State<CountriesList> {
                                 countries[index],
                                 style: TextStyle(color: Colors.white38),
                               ),
-                              tileColor: Colors.blue[100],
+                              //tileColor: Colors.blue[100],
                               trailing: Icon(Icons.arrow_forward_ios),
                             ),
                           );

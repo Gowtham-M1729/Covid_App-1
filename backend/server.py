1c12254d2003eb070daedd362a8a8bb9dce8b74d
Gowtham M <55031924+Gowtham-M1729@users.noreply.github.com>
2021-09-18 14:53:16 +0530

    Adds Global data and  home page

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, jsonify
+from flask import Flask, send_file, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy, inspect
 import requests
 import json
@@ -35,6 +35,24 @@ class DataModel(db.Model):
         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
 
 
+class Global(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    country = db.Column(db.String(100), nullable=False)
+    newConfirmed = db.Column(db.Integer, nullable=False)
+    newDeaths = db.Column(db.Integer, nullable=False)
+    newRecovered = db.Column(db.Integer, nullable=False)
+    totalConfirmed = db.Column(db.Integer, nullable=False)
+    totalDeaths = db.Column(db.Integer, nullable=False)
+    totalRecovered = db.Column(db.Integer, nullable=False)
+    active = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Data(name = {self.country},newconfiremd = {self.newConfirmed}, newdeaths = {self.newDeaths}, newrecovered = {self.newRecovered},totalconfirmed={self.totalConfirmed},totalrecovered={self.totalRecovered},totaldeaths={self.totalDeaths},active = {self.active})"
+
+    def dict(self):
+        return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+
+
 def updateDatabase():
 
     print("Updating database ....")
@@ -42,9 +60,8 @@ def updateDatabase():
     lst_countrysummary = (requests.get(
         "https://api.covid19api.com/summary")).json()
     lst = lst_countrysummary["Countries"]
-    #countries = sorted(lst, key=lambda c: c["NewConfirmed"])
-    globaldata = lst_countrysummary['Global']
-
+    # countries = sorted(lst, key=lambda c: c["NewConfirmed"])
+    globalactivecase = 0
     for i in lst:
         (country, countrycode, slug, newconfirmed, totalconfirmed, newdeaths, totaldeaths, newrecovered, totalrecovered,
          date) = i['Country'], i['CountryCode'], i['Slug'], i['NewConfirmed'], i['TotalConfirmed'], i['NewDeaths'], i['TotalDeaths'], i['NewRecovered'], i['TotalRecovered'], i['Date']
@@ -57,22 +74,48 @@ def updateDatabase():
         c_data = DataModel(iso2=countrycode, country=country, slug=slug, newConfirmed=newconfirmed, newDeaths=newdeaths, newRecovered=newrecovered, totalConfirmed=totalconfirmed,
                            totalRecovered=totalrecovered, totalDeaths=totaldeaths, active=activeCases)
         db.session.add(c_data)
+        globalactivecase += activeCases
 
         print("Updated " + country)
         db.session.commit()
         time.sleep(1)
 
+    globaldata = lst_countrysummary['Global']
+
+    (country, newconfirmed, totalconfirmed, newdeaths, totaldeaths, newrecovered, totalrecovered,
+     date) = "Global", globaldata['NewConfirmed'], globaldata['TotalConfirmed'], globaldata['NewDeaths'], globaldata['TotalDeaths'], globaldata['NewRecovered'], globaldata['TotalRecovered'], globaldata['Date']
+
+    c_data = Global(country=country, newConfirmed=newconfirmed, newDeaths=newdeaths, newRecovered=newrecovered, totalConfirmed=totalconfirmed,
+                    totalRecovered=totalrecovered, totalDeaths=totaldeaths, active=globalactivecase)
+    db.session.add(c_data)
+    db.session.commit()
+    print("Updated Global Data")
+
     print("Update complete ...")
 
 
-@app.route('/reset')
+@ app.route('/reset')
 def reset():
     db.drop_all()
     db.create_all()
     return "Done"
 
 
-@app.route('/countryList')
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@ app.route('/Global')
+def globalinfo():
+    try:
+        data = (Global.query.filter_by(country='Global').first())
+        return jsonify(data.dict())
+    except:
+        return jsonify({'message': 'Global information not found'})
+
+
+@ app.route('/countryList')
 def countrylist():
     try:
         lst = []
@@ -84,7 +127,7 @@ def countrylist():
         return str(e)
 
 
-@app.route('/country')
+@ app.route('/country')
 def index():
     try:
         data = DataModel.query.all()
@@ -93,7 +136,7 @@ def index():
         return str(e)
 
 
-@app.route('/country/<string:name>')
+@ app.route('/country/<string:name>')
 def countryinfo(name):
     try:
         data = DataModel.query.filter(DataModel.country == name).first()
@@ -104,13 +147,13 @@ def countryinfo(name):
 
 if __name__ == "__main__":
     db.create_all()
+
     thread = threading.Thread(target=updateDatabase)
     thread.start()
-
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=updateDatabase, trigger="interval", days=1)
     scheduler.start()
 
     app.run(debug=True, use_reloader=False,
             port=os.getenv("PORT"), host="0.0.0.0")
-    #atexit.register(lambda: scheduler.shutdown())
+    atexit.register(lambda: scheduler.shutdown())
